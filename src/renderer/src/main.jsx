916ee851ca8158960edfa2f5f9f9fa46e8ba3c2f
Vitya <viktorpiros4@mail.ru>
2025-02-12 17:57:28 +0300

    2 modules

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -1,11 +1,19 @@
 import './styles.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Routes, Route, HashRouter } from 'react-router'
+import App from './App.jsx'
+import UpdatePartner from './UpdatePartner.jsx'
+import CreatePartner from './CreatePartner.jsx'
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <HashRouter>
+    <StrictMode>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='/update' element={<UpdatePartner/>}/>
+        <Route path='/create' element={<CreatePartner/>}/>
+      </Routes>
+    </StrictMode>
+  </HashRouter>
 )
